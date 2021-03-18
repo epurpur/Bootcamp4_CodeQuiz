b04ed811ab1ec411d281ee86a08bfb4fef62cfae
Erich Purpur <epurpur@gmail.com>
2021-03-18 17:19:28 -0400

    adding timer functionality

--- assets/js/highscore-page.js
+++ assets/js/highscore-page.js
@@ -21,3 +21,67 @@ playAgainBtn.addEventListener("click", function() {
 });
 
 displayScores();
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~
+
+
+
+
+// // You need to remember the function you used with `addEventListener`, then use that same function with `removeEventListener`.
+
+// var answerChoice = document.querySelectorAll(".answer-choice");    //this is a nodelist of 4 HTML elements
+
+// function evaluateUserChoice(randomQ) {
+//     // takes random question created from makeQuestion()
+//     // evaluates if user clicks on correct answer or not
+
+//     for (var i = 0; i < answerChoice.length; i++) {
+//         // *** NOTE: Must be `const` or `let`, not `var`
+//         const fn = function(event) {
+//             var element = event.target;
+//             //checking to see if user choice is correct
+//             if (element.textContent === randomQ.correct) {
+//                 score++;
+//                 numberCorrect.textContent = score;
+//                 answerChoice[i].removeEventListener("click", fn); // ***
+//                 makeQuestion();                     // resets text content of HTML
+//             } else {
+//                 answerChoice[i].removeEventListener("click", fn); // ***
+//                 makeQuestion();                     // resets text content of HTML
+//             }    
+//         };
+//         answerChoice[i].addEventListener("click", fn);
+//     }
+// }
+
+// // BUT, you don't have to create a new function every time. Instead:
+
+// var answerChoice = document.querySelectorAll(".answer-choice");    //this is a nodelist of 4 HTML elements
+
+// function choiceClickHandler(event) {
+//     var element = event.target;
+//     //checking to see if user choice is correct
+//     if (element.textContent === randomQ.correct) {
+//         score++;
+//         numberCorrect.textContent = score;
+//         this.removeEventListener("click", choiceClickHandler); // *** `this` is the element you hooked the handler to
+//         makeQuestion();                     // resets text content of HTML
+//     } else {
+//         this.removeEventListener("click", choiceClickHandler); // *** `this` is the element you hooked the handler to
+//         makeQuestion();                     // resets text content of HTML
+//     }    
+// }
+
+// function evaluateUserChoice(randomQ) {
+//     // takes random question created from makeQuestion()
+//     // evaluates if user clicks on correct answer or not
+
+//     for (var i = 0; i < answerChoice.length; i++) {
+//         answerChoice[i].addEventListener("click", choiceClickHandler);
+//     }
+// }
--- assets/js/index-script.js
+++ assets/js/index-script.js
@@ -11,6 +11,8 @@ var finalScoreValue = document.getElementById('final-score-value');
 var submissionForm = document.getElementById('submission-form');
 var userName = document.getElementById('user-name');
 var submitBtn = document.getElementById('btnSubmitScore');
+var timerElement = document.getElementById('timer-element');
+var timeCount = document.getElementById('time-count');
 
 /** holds all questions used in the quiz. Stores an array of objects. Each question is an object.
  *  Each question has three properties:
@@ -21,15 +23,32 @@ var submitBtn = document.getElementById('btnSubmitScore');
 var questionBank = [
     {qTitle:"What is the name of a container for storing a data value in Javascript?", answers: ["element", "variable", "document", "viewport"], correct: "variable"},
     {qTitle:"In javascript, what is an ordered list of values called?", answers: ["array", "object", "loop", "html"], correct: "array"},
-    // {qTitle:"What is the name for the language used to describe the presentation of a document?", answers: ["html", "javascript", "css", "python"], correct: "css"},
-    // {qTitle:"What is the name of the version control software we use in this course?", answers: ["bitbucket", "vs code", "terminal", "git"], correct: "git"},
-    // {qTitle:"Which layout tool allows us easy, flexible design of our html elements?", answers: ["javascript", "flexbox", "css", "github"], correct: "flexbox"},
-    // {qTitle:"What property of the window object allows us to save information, even after a browser refresh?", answers: ["getItem", "textContent", "addEventListener", "localStorage"], correct: "localStorage"},
-    // {qTitle:"In javascript, what is the name for a reusable piece of code written to perform a specific task?", answers: ["function", "object", "string", "variable"], correct: "function"}
+    {qTitle:"What is the name for the language used to describe the presentation of a document?", answers: ["html", "javascript", "css", "python"], correct: "css"},
+    {qTitle:"What is the name of the version control software we use in this course?", answers: ["bitbucket", "vs code", "terminal", "git"], correct: "git"},
+    {qTitle:"Which layout tool allows us easy, flexible design of our html elements?", answers: ["javascript", "flexbox", "css", "github"], correct: "flexbox"},
+    {qTitle:"What property of the window object allows us to save information, even after a browser refresh?", answers: ["getItem", "textContent", "addEventListener", "localStorage"], correct: "localStorage"},
+    {qTitle:"In javascript, what is the name for a reusable piece of code written to perform a specific task?", answers: ["function", "object", "string", "variable"], correct: "function"}
 ]
 
-//accumulator variable which counts correct answer choices
-score = 0;
+// accumulator variable which counts correct answer choices
+var score = 0;
+
+
+// global timer variable
+var timeleft = 3;
+
+function timer() {
+    // starts timer countdown starting from value of timeLeft
+    var countDown = setInterval(function() {
+        timeleft--;
+        timeCount.textContent = timeleft;
+
+        if (timeleft <= 0) {
+            clearInterval(countDown);   // stops at 0
+        }
+    }, 1000);    //counts in milliseconds. 1000ms = 1second
+}
+
 
 
 function randomQuestion() {
@@ -57,10 +76,46 @@ function makeQuestion() {
     qIndex = questionBank.indexOf(randomQ);
     questionBank.splice(qIndex, 1);
     
-    //answer is evaluated
     evaluateUserChoice(randomQ);
-
 }
+
+
+
+
+
+
+// function evaluateUserChoice(randomQ) {
+//     // takes random question created from makeQuestion()
+//     // evaluates if user clicks on correct answer or not
+    
+//     if (timeleft > 0) {      // if there is still time left on the clock
+//         for (var i = 0; i < answerChoice.length; i++) {
+//             answerChoice[i].addEventListener("click", function(event) {
+//                 var element = event.target;
+//                 //checking to see if user choice is correct
+//                 if (element.textContent === randomQ.correct) {
+//                     score++;
+//                     numberCorrect.textContent = score;
+//                     // answerChoice[i].removeEventListener("click", fn);
+//                     if (questionBank.length > 0) {   //if there are any questions remaining in the questionBank
+//                         makeQuestion();   //make next question
+//                     } else {                         //if there are no questions remaining in the questionBank
+//                         setFinalStyling();   //set final styles of page; 
+//                     }
+//                 } else {
+//                     // answerChoice[i].removeEventListener("click", fn);
+//                     if (questionBank.length > 0) {
+//                         makeQuestion();
+//                     } else {
+//                         setFinalStyling();
+//                     }
+//                 }    
+//             });
+//         }
+//     } else {       //if no time remaining, skip straight to final styling upon click
+//         setFinalStyling();
+//     }
+// }
 
 
 function evaluateUserChoice(randomQ) {
@@ -68,21 +123,41 @@ function evaluateUserChoice(randomQ) {
     // evaluates if user clicks on correct answer or not
 
     for (var i = 0; i < answerChoice.length; i++) {
-        answerChoice[i].addEventListener("click", function(event) {
+        // *** NOTE: Must be `const` or `let`, not `var`
+        console.log('outer', answerChoice[i]);
+        const fn = function(event) {
+            console.log(answerChoice[i]);
             var element = event.target;
             //checking to see if user choice is correct
             if (element.textContent === randomQ.correct) {
                 score++;
                 numberCorrect.textContent = score;
-                if (questionBank.length > 0) {   //if there are any questions remaining in the questionBank
-                    makeQuestion();   //make next question
-                } else {                         //if there are no questions remaining in the questionBank
-                    setFinalStyling();   //set final styles of page; 
-                }
-            }    //HERE I SHOULD BE ABLE TO RUN makeQuestion(); 
-        });
+                answerChoice[i].removeEventListener("click", fn); // ***
+                makeQuestion();                     // resets text content of HTML
+            } else {
+                answerChoice[i].removeEventListener("click", fn); // ***
+                makeQuestion();                     // resets text content of HTML
+            }    
+        };
+        answerChoice[i].addEventListener("click", fn);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 function setFinalStyling() {
@@ -94,22 +169,32 @@ function setFinalStyling() {
 
     // show final score
     finalScoreValue.textContent = score;
+
+    // set timer to display final message
+    timerElement.textContent = "No Time Remaining!";
 }
 
 
 // starts with invisible questions and final input form, when start button is clicked, questions become visible and a question appears
 questions.style.visibility = "hidden";
 finalInputForm.style.visibility = "hidden";
+timerElement.style.visibility = "hidden"
+
 startBtn.addEventListener('click', function(event) {
-        console.log('clicked');    
         //button becomes invisible upon click
         startBtn.style.visibility = "hidden";
 
         //questions become visible upon click
         questions.style.visibility = "visible";
 
+        //timer becomes visible
+        timerElement.style.visibility = "visible";
+
         //put question on user interface and starts the game
         makeQuestion();
+
+        //start game timer
+        timer();
 });
 
 
@@ -156,6 +241,15 @@ submissionForm.addEventListener('submit', function(event) {
 });
 
 
+
+var quizTitle = document.getElementById('quiz');
+var listenerExists = false;
+
+quizTitle.addEventListener("click", function() {
+    console.log('title click');
+    listenerExists = true;
+    console.log('Listener Exists?: ',listenerExists);
+});
 
 
 
